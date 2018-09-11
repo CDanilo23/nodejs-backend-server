8ccb6fb147fe9949096642effbf67064b8a9e601
Cristian Danilo Ordoñez <cristian.ordonez@globant.com>
2018-09-11 16:59:35 -0500

    adding valitation in user

--- models/user.js
+++ models/user.js
@@ -1,15 +1,21 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
 
+var validRoles = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} is not a right rol'
+};
 
 var userSchema = new Schema({
     name: { type: String, required: [true, 'The name is necessary'] },
-    email: {type:String, required: [true, "The email is necessary"]},
+    email: {type:String, unique: true, required: [true, "The email is necessary"]},
     password: {type: String, required: [true, 'The password is necessary']},
     img: {type: String, required: false},
-    role: {type: String, required: true, default: 'USER_ROLE' }
+    role: {type: String, required: true, default: 'USER_ROLE', enum: validRoles }
 });
 
+userSchema.plugin( uniqueValidator, {message: 'The {PATH} must be unique'});
 
 module.exports = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,10 @@ var app = express();
 
 var User = require('../models/user');
 
+//================================
+// Get all users
+//================================
+
 app.get('/', (req, res, next) => {
 
     User.find({}, 'name email img role')
@@ -26,5 +30,40 @@ app.get('/', (req, res, next) => {
     });
 
 });
+
+//================================
+// Create new user
+//================================
+
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var user = new User({
+        name: body.name,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    user.save((err, userSaved) => {
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                message: 'Error saving user',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            user: userSaved,
+            });          
+    });
+
+
+} );
 
 module.exports = app;
